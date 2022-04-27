ed2c1d979e00821e4d8e692436505071c95fbcc2
ouze92 <71232544+ouze92@users.noreply.github.com>
2022-04-27 09:30:14 +0100

    Update best.py

--- best.py
+++ best.py
@@ -44,6 +44,7 @@ def linex():
 	os.system('echo  "\n ↠↠↠↠↠↠↠↠↠↠↠↠↠\n" | lolcat -a -d 2 -s 50')
 def logo():
 	os.system('echo "\n 
+		  
 ░█████╗░██╗░░░░░██╗
 ██╔══██╗██║░░░░░██║
 ███████║██║░░░░░██║
